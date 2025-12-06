80de8edcff83ad852a3d03a9dd1472cd6d0a466d
tuorqai <tuorqai@gmail.com>
2025-12-06 12:59:43 +0900

    Firewall management implementation for Linux (requires lots of error checking and other improvements)

--- LinuxFirewallManager.cpp
+++ LinuxFirewallManager.cpp
@@ -3,6 +3,9 @@
 
 #include "FirewallManager.h"
 
+#include <nftables/libnftables.h>
+#include <nlohmann/json.hpp>
+
 //------------------------------------------------------------------------------
 
 class LinuxFirewallManager : public FirewallManager
@@ -19,16 +22,29 @@ public:
 
     void Clear() override;
     bool IsClear() override;
+
+private:
+    void CheckTables();
+
+    nft_ctx *m_nftCtx;
+    std::vector<wxString> m_definedRules;
 };
 
 //------------------------------------------------------------------------------
 
 LinuxFirewallManager::LinuxFirewallManager()
 {
+    m_nftCtx = nft_ctx_new(NFT_CTX_DEFAULT);
+
+    nft_ctx_buffer_output(m_nftCtx);
+    nft_ctx_output_set_flags(m_nftCtx, NFT_CTX_OUTPUT_JSON);
+
+    CheckTables();
 }
 
 LinuxFirewallManager::~LinuxFirewallManager()
 {
+    nft_ctx_free(m_nftCtx);
 }
 
 bool LinuxFirewallManager::IsFirewallEnabled()
@@ -38,24 +54,91 @@ bool LinuxFirewallManager::IsFirewallEnabled()
 
 bool LinuxFirewallManager::IsLocationBlocked(ServerData::Location const &location)
 {
-    return false;
+    wxString rule = wxString::Format("deadlock_%s", location.identifier);
+
+    return std::find(m_definedRules.begin(), m_definedRules.end(), rule) != m_definedRules.end();
 }
 
 void LinuxFirewallManager::BlockLocation(ServerData::Location const &location)
 {
+    wxString rule = wxString::Format("deadlock_%s", location.identifier);
+
+    nft_run_cmd_from_buffer(m_nftCtx, wxString::Format(
+        "add table inet %s", rule
+    ));
+
+    nft_run_cmd_from_buffer(m_nftCtx, wxString::Format(
+        "add chain inet %s output { type filter hook output priority 0; }",
+        rule
+    ));
+
+    for (size_t i = 0; i < location.relays.size(); i++) {
+        nft_run_cmd_from_buffer(m_nftCtx, wxString::Format(
+            "add rule inet %s output ip daddr %s drop",
+            rule, location.relays[i].ipv4
+        ));
+    }
+
+    m_definedRules.emplace_back(rule);
 }
 
 void LinuxFirewallManager::UnblockLocation(ServerData::Location const &location)
 {
+    wxString rule = wxString::Format("deadlock_%s", location.identifier);
+
+    int rc = nft_run_cmd_from_buffer(m_nftCtx, wxString::Format(
+        "destroy table inet %s", rule
+    ));
+
+    if (rc == 0) {
+        m_definedRules.erase(std::find(m_definedRules.begin(), m_definedRules.end(), rule));
+    }
 }
 
 void LinuxFirewallManager::Clear()
 {
+    CheckTables(); // just in case(?)
+
+    for (wxString const &rule : m_definedRules) {
+        nft_run_cmd_from_buffer(m_nftCtx, wxString::Format(
+            "destroy table inet %s", rule
+        ));
+    }
+
+    m_definedRules.clear();
 }
 
 bool LinuxFirewallManager::IsClear()
 {
-    return true;
+    return m_definedRules.empty();
+}
+
+void LinuxFirewallManager::CheckTables()
+{
+    m_definedRules.clear();
+
+    nft_run_cmd_from_buffer(m_nftCtx, "list tables inet");
+
+    auto output = nlohmann::json::parse(nft_ctx_get_output_buffer(m_nftCtx));
+    auto nftables = output["nftables"];
+
+    if (nftables.is_null()) {
+        return;
+    }
+
+    for (auto item : nftables) {
+        auto table = item["table"];
+
+        if (table.is_null()) {
+            continue;
+        }
+
+        wxString name = table["name"].get<std::string>();
+
+        if (name.StartsWith("deadlock_")) {
+            m_definedRules.emplace_back(name);
+        }
+    }
 }
 
 //------------------------------------------------------------------------------
